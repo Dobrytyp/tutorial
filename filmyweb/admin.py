67384b48e97388ad244e82df8dbf4c70dffc8c79
Dobrytyp <dobrytyp@gmail.com>
2020-04-05 18:42:49 +0200

    05.04.2020

--- filmyweb/admin.py
+++ filmyweb/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Film
 
-# Register your models here.
+
+# admin.site.register(Film)     # Prostszy sposób
+
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    # fields = ["tytul", "opis", "rok"]
+    # exclude = ["opis"]
+    list_display = ["tytul", "imdb_rating", "rok"]
+    list_filter = ("rok",)      # Na końcu krotki musi być przecinek!
+    search_fields = ('tytul', 'opis')  # Na końcu krotki musi być przecinek!

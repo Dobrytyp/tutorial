1ea922422df2168318a0876282df85022b3cfa3b
Dobrytyp <dobrytyp@gmail.com>
2020-04-06 18:52:03 +0200

    06.04.2020

--- filmyweb/views.py
+++ filmyweb/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Film
 
 # Create your views here.
-def test_response(request):
-    return HttpResponse('To jest nasz pierwszy test')
+def wszystkie_filmy(request):
+    wszystkie = Film.objects.all
+    # return HttpResponse('<h1>To jest nasz pierwszy test</h1>')
+    return render(request,  'filmy.html', {'filmy': wszystkie})

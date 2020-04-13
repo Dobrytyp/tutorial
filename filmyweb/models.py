3dc94b3f6385be63687050926bb25bbb1974d4c7
Dobrytyp <dobrytyp@gmail.com>
2020-04-13 18:39:33 +0200

    13.04.2020

--- filmyweb/models.py
+++ filmyweb/models.py
@@ -10,7 +10,6 @@ class Film(models.Model):
     premiera = models.DateField(null=True, blank=True)
     imdb_rating = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
     plakat = models.ImageField(upload_to="plakaty", null=True, blank=True)
-    gatunek = models.CharField(max_length=32, null=True, blank=True)
 
     def __str__(self):
         return self.tytul_z_rokiem()  # Tu będzie się wyswietlało pole tytuł i rok
--- filmyweb/views.py
+++ filmyweb/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Film
+from .forms import FilmForm
 
 # Create your views here.
 def wszystkie_filmy(request):
     wszystkie = Film.objects.all()
     # return HttpResponse('<h1>To jest nasz pierwszy test</h1>')
     return render(request,  'filmy.html', {'filmy': wszystkie})
+
+def nowy_film(request):
+    form = FilmForm(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        form.save()
+
+    return render(request, 'nowy_film.html', {'form': form})
+
+
